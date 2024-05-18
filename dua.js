3d640ca268a63709fe04b3539a6567ef43eb5fb8
panduyogaw <panduyoga74@gmail.com>
2024-05-18 12:48:04 +0700

    pandu yoga

---
+++ dua.js
@@ -0,0 +1,9 @@
+
+// const fs = require('fs');
+// const cetakNama = require('coba'); // local moodule
+// const moment = require('moment');
+
+
+const latihan = require('./latihan');
+
+console.log(latihan.cetakNama('pandu'), latihan.PI, latihan.mahasiswa.cetakMhs());
